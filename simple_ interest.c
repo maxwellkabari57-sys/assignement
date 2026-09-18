07385f41ac3f8e3f112f5277a0a04b096d8745cb
maxwellkabari57-sys <maxwellkabari57@gmail.com>
2026-09-18 17:38:45 +0300

    Add files via upload

---
+++ simple_ interest.c
@@ -0,0 +1,27 @@
+//a program for calculating simple inerest
+
+#include<stdio.h>
+
+int main(){
+	
+	float rate,time,principal,simpleinterest;
+	
+	printf("Enter principle: \n"); // principal amount %f
+	scanf("%f",&principal);
+	
+	printf("Enter rate: \n"); //rate %f
+	scanf("%f",&rate);
+	
+	printf("Enter time(in year): \n"); //time %f
+	scanf("%f",&time);
+	
+	simpleinterest= (principal*time*rate)/100.0 ;
+	
+	printf("simple interest = %.2f\n" , simpleinterest);
+	
+	
+	
+	
+	
+	return 0;
+}
