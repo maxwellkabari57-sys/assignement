ab0a925f4127d960a436fe8a225479edc4a222a9
maxwellkabari57-sys <maxwellkabari57@gmail.com>
2026-09-22 12:28:55 +0300

    Add files via upload

---
+++ units taken 1.1 .c
@@ -0,0 +1,16 @@
+// units taken
+
+#include<stdio.h>
+
+int main(){
+	
+
+	
+	printf("Introduction to programming\n");
+	printf("Data structures \n");
+	printf("Computer networks\n");
+	printf("Database systems \n");
+	printf("Software engineering \n");
+	
+	return 0;
+}
